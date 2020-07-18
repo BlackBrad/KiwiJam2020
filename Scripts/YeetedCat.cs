86728a680e069dae7ea0158a20e495aad393aef4
David Goosen <david@davidgoosen.kiwi>
2020-07-18 20:36:48 +1200

    Initial audio integration for cat yeeting

--- Scripts/YeetedCat.cs
+++ Scripts/YeetedCat.cs
@@ -8,6 +8,16 @@ public class YeetedCat : KinematicBody2D
     [Export]
     private Vector2 _InitialVelocity = new Vector2();
 
+    private AudioStreamPlayer2D _AudioStreamPlayer;
+
+    public override void _Ready()
+    {
+        _AudioStreamPlayer = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
+
+        _AudioStreamPlayer.PitchScale = (float)GD.RandRange(0.9f, 1.1f);
+        _AudioStreamPlayer.VolumeDb = (float)GD.RandRange(0.9f, 1.2f);
+    }
+
     public override void _PhysicsProcess(float delta)
     {
         var rel = _Velocity * delta;
